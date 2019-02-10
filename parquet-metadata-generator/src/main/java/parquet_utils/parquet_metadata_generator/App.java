8d17aa81d165dd05d35696fead3f0548586a7503
MongJu Jung <mjjung@gmail.com>
2019-02-10 17:45:58 +0900

    initial commit

---
+++ parquet-metadata-generator/src/main/java/parquet_utils/parquet_metadata_generator/App.java
@@ -0,0 +1,25 @@
+package parquet_utils.parquet_metadata_generator;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.parquet.hadoop.ParquetFileReader;
+import org.apache.parquet.hadoop.ParquetFileWriter;
+
+public class App {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			System.err.println("Correct Usage: ./parquet-metadata-generator.jar <hdfs://..|file:///..>");
+			System.exit(1);
+		}
+		
+		Path dir = new Path(args[0]);
+		Configuration conf = new Configuration();
+		FileSystem fs = dir.getFileSystem(conf);
+		
+		ParquetFileWriter.writeMetadataFile(conf, dir, ParquetFileReader.readAllFootersInParallel(conf, fs.getFileStatus(dir)));
+	}
+}
